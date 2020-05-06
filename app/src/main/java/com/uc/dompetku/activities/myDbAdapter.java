663a24fbb0b4b66b00a11407937cd0f719a352d8
Likokas <62798918+Likokas@users.noreply.github.com>
2020-05-06 20:26:34 +0800

    DATABASE SQLITE

---
+++ app/src/main/java/com/uc/dompetku/activities/myDbAdapter.java
@@ -0,0 +1,90 @@
+package com.uc.dompetku.activities;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.uc.dompetku.fragments.TransaksiFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class myDbAdapter {
+
+    myDbHelper myhelper;
+
+    public myDbAdapter(Context context)
+    {
+        myhelper = new myDbHelper(context);
+    }
+
+    public long insertData(String name, String umur, String alamat)
+    {
+        SQLiteDatabase dbb = myhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(myDbHelper.TANGGAL, name);
+        contentValues.put(myDbHelper.KATEGORI, umur);
+        contentValues.put(myDbHelper.JUMLAH, alamat);
+        long id = dbb.insert(myDbHelper.TABLE_NAME, null , contentValues);
+        return id;
+    }
+
+//
+
+    public List<User> allPlayers() {
+        List<User> users = new ArrayList<>();
+        String selectQuery = "SELECT * FROM user";
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Log.e("name", cursor.getString(1));
+                User user = new User(cursor.getString(1),cursor.getString(3),cursor.getString(2));
+
+                users.add(user);
+            } while (cursor.moveToNext());
+        }
+
+        db.close();
+        Log.e("list", users.toString());
+        return users;
+    }
+
+
+    static class myDbHelper extends SQLiteOpenHelper
+    {
+        private static final String DATABASE_NAME = "myDatabase";    // Database Name
+        private static final String TABLE_NAME = "user";   // Table Name
+        private static final int DATABASE_Version = 1;   // Database Version
+        private static final String UID="_id";
+        private static final String TANGGAL = "tanggal";
+        private static final String KATEGORI = "kategori";
+        private static final String JUMLAH = "jumlah";
+        private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+
+                " ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+TANGGAL+" VARCHAR(255) ,"+ KATEGORI+" VARCHAR(255), "+JUMLAH+" VARCHAR(225));";
+        private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+TABLE_NAME;
+        private Context context;
+
+        public myDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_Version);
+            this.context=context;
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+
+            try {
+                db.execSQL(CREATE_TABLE);
+            } catch (Exception e) {
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
